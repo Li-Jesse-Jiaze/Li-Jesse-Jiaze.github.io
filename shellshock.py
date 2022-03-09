070a434fc7ef72e597f03f64a39a783a275d3830
Jesse Lee <963204825@qq.com>
2022-03-09 21:15:41 +0800

    Site updated: 2022-03-09 21:15:41

--- shellshock.py
+++ shellshock.py
@@ -1,18 +1,14 @@
+import time
+from os import system
 import math
 from pynput.mouse import Listener as MouseListener
-from pynput import keyboard
 import pynput.keyboard as kb
-import pynput.mouse as ms
-import time
+import pyautogui
 import platform
-from os import system
+
 
 keys = kb.Controller()
-mouse = ms.Controller()
 os_name = platform.system()
-
-global set_wind
-set_wind = ""
 
 
 # define our clear function
@@ -33,61 +29,28 @@ def calcVelocity(x_diff, y_diff, angle):
     q = 0.0518718
     try:
         v0 = -2 / (g * q) * math.sqrt((-g * x_diff * x_diff) / (
-                    2 * math.cos(math.radians(angle)) * math.cos(math.radians(angle)) * (
-                        math.tan(math.radians(angle)) * x_diff - y_diff)))
+                2 * math.cos(math.radians(angle)) * math.cos(math.radians(angle)) * (
+                math.tan(math.radians(angle)) * x_diff - y_diff)))
     except ZeroDivisionError:
         v0 = 0
     return v0
 
 
-# calculate velocity with wind
-def calcVelocityWithWind(x_diff, y_diff, angle, wind):
-    g = 379.106
-    q = 0.0518718
-    # z = 0.4757
-    # z = 0.513538766675914
-    z = 0.5
-    w = z * wind
-    v_0 = (g * x_diff - w * y_diff) / (math.sqrt(
-        2 * g * x_diff * math.sin(math.radians(angle)) * math.cos(math.radians(angle)) + 2 * g * y_diff * pow(
-            math.cos(math.radians(angle)), 2) + 2 * w * x_diff * pow(math.sin(math.radians(angle)),
-                                                                     2) + 2 * w * y_diff * math.sin(
-            math.radians(angle)) * math.cos(math.radians(angle))))
-    power = (2 / (g * q)) * v_0
-    return power
-
-
-def calcVelocityWithWind_y0(x_diff, angle, wind):
-    g = 379.106
-    q = 0.0518718
-    z = 0.4757
-    w = z * wind
-    print("Inputs: " + str(x_diff) + ", " + str(angle) + ", " + str(wind))
-    v_0 = (g * math.sqrt(x_diff)) / (math.sqrt(
-        2 * g * math.sin(math.radians(angle)) * math.cos(math.radians(angle)) + 2 * w * pow(
-            math.sin(math.radians(angle)), 2)))
-    power = (2 / (g * q)) * v_0
-    print(power)
-    return power
-
-
-def calcOptimal(x_diff, y_diff, wind):
+def calcOptimal(x_diff, y_diff):
     smallestVelocity = 100
     bestAngle = 0
     global velocity
     global angle
     for possibleAngle in range(1, 90):
         try:
-            if wind == 0:
-                v0 = calcVelocity(x_diff, y_diff, possibleAngle)
-            else:
-                v0 = calcVelocityWithWind(x_diff, y_diff, possibleAngle, wind)
+            v0 = calcVelocity(x_diff, y_diff, possibleAngle)
             if v0 < smallestVelocity:
                 smallestVelocity = v0
                 bestAngle = possibleAngle
         except Exception as e:
             pass
     clear()
+    print("∠(･`_´･ ) Sir, we are on target!")
     print("\n------ Smallest -----")
     print("Power\tAngle")
     print("{:.2f}\t {:d}".format(smallestVelocity, bestAngle))
@@ -95,17 +58,11 @@ def calcOptimal(x_diff, y_diff, wind):
     angle = bestAngle
 
 
-def calcHighestBelow100(x_diff, y_diff, wind):
+def calcHighestBelow100(x_diff, y_diff):
     global highVelocity
     global highAngle
     for possibleAngle in range(1, 90):
-        if wind == 0:
-            v0 = calcVelocity(x_diff, y_diff, 90 - possibleAngle)
-        else:
-            try:
-                v0 = calcVelocityWithWind(x_diff, y_diff, 90 - possibleAngle, wind)
-            except Exception as e:
-                v0 = 101
+        v0 = calcVelocity(x_diff, y_diff, 90 - possibleAngle)
         if v0 < 100:
             break
 
@@ -114,6 +71,13 @@ def calcHighestBelow100(x_diff, y_diff, wind):
     print("{:.2f}\t {:d}".format(v0, 90 - possibleAngle))
     highVelocity = v0
     highAngle = 90 - possibleAngle
+    time.sleep(1)
+    is_left = (int(YourX > EnemyX) - 0.5) * 2
+    alpha = 10.0 / 3
+    pyautogui.click(YourX - alpha * is_left * highVelocity * math.cos(math.radians(highAngle)),
+                    YourY - alpha * highVelocity * math.sin(math.radians(highAngle)), button='left')
+    print("\nThe highest trajectory have been automatically selected!\n(automatic selection may have some errors, please check and fine tune)")
+    print("\nlocate your tank(p)/target(e)")
 
 
 def cleanGlobals():
@@ -133,12 +97,6 @@ def cleanGlobals():
         del globals()['EnemyY']
     except Exception as e:
         pass
-    try:
-        del globals()['set_wind']
-    except Exception as e:
-        pass
-
-
 
 
 def posPlayer(x, y, button, pressed):
@@ -162,11 +120,6 @@ def posEnemy(x, y, button, pressed):
 
 
 def PlayerLocation():
-    global set_wind
-    if set_wind == "":
-        wind = 0
-    else:
-        wind = int(set_wind)
     # cleanGlobals()
     print('Click your Tank')
     mouse_listener = MouseListener(on_click=posPlayer)
@@ -176,17 +129,11 @@ def PlayerLocation():
         # all needed, calc shot
         diffx = abs(YourX - EnemyX)
         diffy = -EnemyY + YourY
-        calcOptimal(diffx, diffy, wind)
-        calcHighestBelow100(diffx, diffy, wind)
-        set_wind = ""
+        calcOptimal(diffx, diffy)
+        calcHighestBelow100(diffx, diffy)
 
 
 def EnemyLocation():
-    global set_wind
-    if set_wind == "":
-        wind = 0
-    else:
-        wind = int(set_wind)
     # cleanGlobals()
     print('Click enemy Tank')
     mouse_listener = MouseListener(on_click=posEnemy)
@@ -196,57 +143,11 @@ def EnemyLocation():
         # all needed, calc shot
         diffx = abs(YourX - EnemyX)
         diffy = -EnemyY + YourY
-        calcOptimal(diffx, diffy, wind)
-        calcHighestBelow100(diffx, diffy, wind)
-        set_wind = ""
-
-
-def readWind():
-    global set_wind
-    print("Enter wind strength followed by <ENTER> on your numpad")
-    set_wind = ""
-    with keyboard.Listener(on_press=readNumber) as lst:
-        lst.join()
-    set_wind = int(set_wind)
-    print("Keystroke: " + str(set_wind))
-    time.sleep(0.1)
-    keys.tap(kb.Key.enter)
-
-
-def readNumber(key):
-    global set_wind
-    # print(key.vk)
-    # print(key.char)
-    print(os_name)
-    if os_name == "Linux":
-        if key == kb.Key.enter:
-            print("ENTER pressed")
-            return False
-        if hasattr(key, 'char'):
-            if key.char is None:
-                number = "5"
-            else:
-                number = key.char
-            print(number)
-            set_wind = set_wind + str(number)
-        if hasattr(key, 'char') and key.char == '-' and set_wind == "":
-            set_wind = set_wind + "-"
-            print("")
-    else:
-        if key == kb.Key.enter:
-            print("ENTER pressed")
-            return False
-        if hasattr(key, 'vk') and 96 <= key.vk <= 105:
-            number = key.vk - 96
-            print(number)
-            set_wind = set_wind + str(number)
-        if hasattr(key, 'char') and key.char == '-' and set_wind == "":
-            set_wind = set_wind + "-"
-            print("")
+        calcOptimal(diffx, diffy)
+        calcHighestBelow100(diffx, diffy)
 
 
 if __name__ == "__main__":
-    with keyboard.GlobalHotKeys(
-            {'P': PlayerLocation, 'E': EnemyLocation,
-             '<alt>+W': readWind}) as h:
+    print("locate your tank(p)/target(e)")
+    with kb.GlobalHotKeys({'P': PlayerLocation, 'E': EnemyLocation, '<ctrl>+C':exit}) as h:
         h.join()
